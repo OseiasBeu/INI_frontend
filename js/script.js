8283232254ab12ad16368a03b5f1a561dcbb9871
Oseias Beu <oseiasbeu@outlook.com>
2021-04-14 00:19:11 -0300

    Estruturar o frontend

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const url = 'http://localhost:3009/api/v1';
-// let exibir = document.querySelector('BTN');
-const button = document.querySelector("button");
-const div = document.querySelector('div');
-
-// var h2 = document.createElement('h2')
+// const url = 'http://localhost:3009/api/v1'; # Local 
+const url = 'http://ini-digital.herokuapp.com/api/v1' //Server
+let div = document.querySelector('#eventos');
+let body = document.querySelector('body');
+let exibir = document.querySelector('#BTN');
+var h2 = document.createElement('h2')
 
 
 // Make a request for a user with a given ID
@@ -13,10 +13,36 @@ axios.get(url+'/tarefas')
     // handle success
     for(var key in response.data){
       let value = response.data[key];
-      var newcontent = document.createElement('aside');
+      
+      var aside = document.createElement('aside');
+      aside.classList.add('evento')
+      
+      var linha = document.createElement('div');
+      linha.setAttribute("id","linha-horizontal")
+      
+      var imgEvento = document.createElement('img');
+      imgEvento.setAttribute("id","imagemEvento")
+
+      var dataEvento = document.createElement('h4');
+      dataEvento.setAttribute("id","dataEvento")
+      
+      var categoria = document.createElement('h3')
+      categoria.setAttribute("id","categoria")
+
+      var descricao = document.createElement('span');
+      descricao.setAttribute("id","descricao")
+      
       console.log(value)
-      newcontent.innerHTML =  value['descricao'] +  '\n' + value['data']
-      div.appendChild(newcontent);
+      imgEvento.src ='https://www.plataformamedia.com/wp-content/uploads/2020/06/imagem-preta.jpg' //value['imagem'] //Futura alteração
+      dataEvento.innerHTML =value['data'] //Inserindo a data do evento
+      categoria.innerHTML = value['categoria_id'] //Inserindo a categoria do evento
+      descricao.innerHTML = value['descricao'] //Inserindo a descrição do evento
+      aside.appendChild(imgEvento);
+      aside.appendChild(dataEvento);
+      aside.appendChild(linha);
+      aside.appendChild(categoria);
+      aside.appendChild(descricao);
+      div.appendChild(aside);
     }
   })
   .catch(function (error) {
@@ -28,4 +54,5 @@ axios.get(url+'/tarefas')
   });
 }
 
-button.onclick = exibirTarefas;
+body.onload = exibirTarefas;
+// exibir.onclick = exibirTarefas;
